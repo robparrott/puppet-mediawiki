71ec114d65adffdf7234d9d17cb330a2b16ea95a
Steve Huff <shuff@vecna.org>
2013-08-30 11:16:43 -0400

    Cleaning up trailing whitespace

--- manifests/init.pp
+++ manifests/init.pp
@@ -55,17 +55,17 @@ class mediawiki (
   $tarball_name             = regsubst($tarball_url, '^.*?/(mediawiki-\d\.\d+.*tar\.gz)$', '\1')
   $mediawiki_dir            = regsubst($tarball_url, '^.*?/(mediawiki-\d\.\d+\.\d+).*$', '\1')
   $mediawiki_install_path   = "${web_dir}/${mediawiki_dir}"
-  
+
   # Specify dependencies
   Class['mysql::server'] -> Class['mediawiki']
   Class['mysql::config'] -> Class['mediawiki']
-  
+
   class { 'apache':
     default_vhost => $default_vhost,
   }
   class { 'apache::mod::php': }
-  
-  
+
+
   # Manages the mysql server package and service by default
   class { 'mysql::server':
     config_hash => { 'root_password' => $db_root_password },
@@ -83,8 +83,8 @@ class mediawiki (
     group   => 'root',
     mode    => '0755',
     require => Package[$mediawiki::params::packages],
-  }  
-  
+  }
+
   # Download and install MediaWiki from a tarball
   exec { "get-mediawiki":
     cwd       => $web_dir,
@@ -92,16 +92,16 @@ class mediawiki (
     creates   => "${web_dir}/${tarball_name}",
     subscribe => File['mediawiki_conf_dir'],
   }
-    
+
   exec { "unpack-mediawiki":
     cwd       => $web_dir,
     command   => "/bin/tar -xvzf ${tarball_name}",
     creates   => $mediawiki_install_path,
     subscribe => Exec['get-mediawiki'],
   }
-  
+
   class { 'memcached':
     max_memory => $max_memory,
     max_connections => '1024',
   }
-} 
+}
